de45710b230860cd5e78ce51c86c1fbaf3d2c410
ShyamAnand007 <a007shyam@gmail.com>
2026-09-17 22:16:10 +0530

    tested if teams can be got blah blah- teams are pretty printed

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,39 @@ def home():
 @app.get("/teams")
 def get_teams():
     cursor=connection.cursor()
-    cursor.execute("SELECT * FROM team")
-    teams=cursor.fetchall()
+    cursor.execute("""
+        SELECT team_id,team_name,country,team_logo,stadium
+        FROM team
+    """  )
+
+    rows=cursor.fetchall()
     cursor.close()
+    teams=[]
+    for row in rows:    
+        teams.append({
+            "team_id":row[0],
+            "team_name":row[1],
+            "country":row[2],
+            "team_logo":row[3],
+            "stadium":row[4]
+        })
     return teams
+
+@app.get("/teams/{team_id}")
+def get_team(team_id: int):
+    cursor=connection.cursor()
+    cursor.execute("""
+        SELECT team_id,team_name,country,team_logo,stadium
+        FROM team
+        WHERE team_id=%s
+    """,(team_id,))
+    row=cursor.fetchone()
+    if row is None:
+            return {"error":"NO MATHCING ID FOUND"}
+    return{
+        "team_id":row[0],
+        "team_name":row[1],
+        "country":row[2],
+        "team_logo":row[3],
+        "stadium":row[4]
+    }
